a4c62ac986a85c25744a87e8792129168a51d68b
Bora Poçan <60720346+borapocan@users.noreply.github.com>
2020-10-03 12:23:28 +0300

    Add files via upload

---
+++ Algorithms/Implementation/AppleAndOrange/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+
+namespace AppleAndOrange
+{
+    class Program
+    {
+
+        static int CountFruits(int[] arr, int tree, int s, int t)
+        {
+            int count = 0;
+
+            foreach (int i in arr)
+            {
+                int j = tree + i;
+
+                if (j >= s && j <= t)
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+        static void Main(string[] args)
+        {
+            int[] st = Array.ConvertAll(Console.ReadLine().Split(' '), i => Convert.ToInt32(i));
+
+            int s = st[0]; int t = st[1];
+
+            int[] ab = Array.ConvertAll(Console.ReadLine().Split(' '), i => Convert.ToInt32(i));
+
+            int a = ab[0]; int b = ab[1];
+
+            int[] mn = Array.ConvertAll(Console.ReadLine().Split(' '), i => Convert.ToInt32(i));
+
+            int m = mn[0]; int n = mn[1];
+
+            int[] aTimesM = Array.ConvertAll(Console.ReadLine().Split(' '), i => Convert.ToInt32(i));
+            
+            int[] bTimesN = Array.ConvertAll(Console.ReadLine().Split(' '), i => Convert.ToInt32(i));
+
+            if (m == aTimesM.Length && n == bTimesN.Length)
+            {
+                Console.WriteLine(CountFruits(aTimesM, a, s, t));
+
+                Console.WriteLine(CountFruits(bTimesN, b, s, t));
+            }
+        }
+    }
+}
---
+++ Algorithms/Implementation/Kangaroo/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Kangaroo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] x1v1x2v2 = Array.ConvertAll(Console.ReadLine().Split(' '), i => Convert.ToInt32(i));
+
+            int x1 = x1v1x2v2[0]; int v1 = x1v1x2v2[1]; int x2 = x1v1x2v2[2]; int v2 = x1v1x2v2[3];
+
+            if (v1 == v2)
+            {
+                if (x1 == x2)
+                {
+                    Console.Write("YES");
+                }
+                else
+                {
+                    Console.Write("NO");
+                }
+            }
+            else
+            {
+                int i = Math.Abs((x2 - x1) / (v2 - v1));
+
+                bool satisfied = x1 + (i * v1) == x2 + (i * v2);
+
+                if (satisfied)
+                {
+                    Console.Write("YES");
+                }
+                else
+                {
+                    Console.Write("NO");
+                }
+            } 
+        }
+    }
+}
