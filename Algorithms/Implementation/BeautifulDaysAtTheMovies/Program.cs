52003ae5c7dde39873bdeed512a7d9ac0c413d7b
Bora Poçan <60720346+borapocan@users.noreply.github.com>
2020-10-22 01:36:28 +0300

    Add files via upload

---
+++ Algorithms/Implementation/BeautifulDaysAtTheMovies/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+
+namespace BeautifulDaysAtTheMovies
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] ijk = Array.ConvertAll(Console.ReadLine().Split(' '), x => Convert.ToInt32(x)); int i = ijk[0]; int j = ijk[1]; int k = ijk[2];
+
+            int beautifulDays = 0;
+
+            for (int y = i; y <= j; y++)
+            {
+                string reversedString = new string(y.ToString().Reverse().ToArray());
+
+                int reversedInteger = int.Parse(reversedString);
+
+                int res = Math.Abs(y - reversedInteger);
+
+                if (res % k == 0)
+                {
+                    beautifulDays++;
+
+                }
+            }
+            Console.WriteLine(beautifulDays);
+        }
+    }
+}
