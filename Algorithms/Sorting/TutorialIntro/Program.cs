ad6fc2e9a02960e08e44c39e957a0c4aad6fb77a
Bora Poçan <60720346+borapocan@users.noreply.github.com>
2020-10-24 02:41:49 +0300

    Add files via upload

---
+++ Algorithms/Sorting/TutorialIntro/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace TutorialIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int v = Convert.ToInt32(Console.ReadLine());
+
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            int[] arr = Array.ConvertAll(Console.ReadLine().Split(' '), x => Convert.ToInt32(x));
+
+            for (int i = 0; i < n; i++)
+            {
+                if (arr[i] == v)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
