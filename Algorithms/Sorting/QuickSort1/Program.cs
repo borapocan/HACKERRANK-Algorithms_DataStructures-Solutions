386acd117cd7165da7d34890381cad7b41c78456
Bora Poçan <60720346+borapocan@users.noreply.github.com>
2020-10-24 02:37:25 +0300

    Add files via upload

---
+++ Algorithms/Sorting/QuickSort1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace QuickSort1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int[] arr = Array.ConvertAll(Console.ReadLine().Split(' '), x => Convert.ToInt32(x));
+
+            int p = arr[0];
+
+            List<int> left = new List<int>(); List<int> equal = new List<int>(); List<int> right = new List<int>();
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] < p)
+                {
+                    left.Add(arr[i]);
+                }
+                else if (arr[i] > p)
+                {
+                    right.Add(arr[i]);
+                }
+                else
+                {
+                    equal.Add(arr[i]);
+                }
+            }
+
+            left.AddRange(equal); left.AddRange(right);
+
+            Console.WriteLine("{0}", string.Join(" ", left));
+        }
+    }
+}
