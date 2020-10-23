620d869a7aa3006edd0ec3f3935eb7b86a01c259
Bora Poçan <60720346+borapocan@users.noreply.github.com>
2020-10-24 02:39:00 +0300

    Add files via upload

---
+++ Algorithms/Sorting/Marc'sCakewalk/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+
+namespace MarcsCakewalk
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            int[] c = Array.ConvertAll(Console.ReadLine().Split(' '), x => Convert.ToInt32(x));
+
+            int[] cSorted = c.OrderByDescending(y => y).ToArray();
+
+            double count = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                double sum = Math.Pow(2.0, i) * cSorted[i];
+
+                count += sum;
+            }
+
+            Console.WriteLine(count);
+
+        }
+
+    }
+
+}
