d071ac4e15e0e9be8c05568870e76ea60b9cfc62
Bora Poçan <60720346+borapocan@users.noreply.github.com>
2020-11-13 01:46:52 +0300

    Add files via upload

---
+++ Algorithms/Sorting/InsertionSortPart2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace InsertionSortPart2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            int[] arr = Array.ConvertAll(Console.ReadLine().Split(" "), x => Convert.ToInt32(x));
+
+            for (int i = 1; i < n; i++)
+            {
+                int last = arr[i];
+
+                for (int j = i; j > 0; j--)
+                {
+                    if (arr[j - 1] > last)
+                    {
+                        arr[j] = arr[j - 1];
+                    }
+                    else
+                    {
+                        arr[j] = last;
+
+                        break;
+                    }
+                }
+
+                if (last < arr[0])
+                {
+                    arr[0] = last;
+                }
+
+                Console.WriteLine(String.Join(" ", arr));
+            }
+        }
+    }
+}
