1629baadf47556faf46ea163e4961a99e1c33f35
Bora Poçan <60720346+borapocan@users.noreply.github.com>
2020-11-13 01:27:25 +0300

    Add files via upload

---
+++ Algorithms/Sorting/InsertionSort/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace InsertionSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            int[] arr = Array.ConvertAll(Console.ReadLine().Split(), x => Convert.ToInt32(x));
+
+            int i, j, temp;
+
+            for (i = 0; i < n; i++)
+            {
+                temp = arr[i];
+
+                for (j = i - 1; j >= 0 && arr[j] > temp; j--)
+                {
+                    arr[j + 1] = arr[j];
+
+                    Console.WriteLine("{0}", string.Join(" ", arr));
+                }
+
+                arr[j + 1] = temp;
+            }
+
+            Console.WriteLine("{0}", string.Join(" ", arr));
+        }
+
+    }
+
+}
